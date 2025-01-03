4f56d3c6f6a6ccfb2a138670b39a4c982d165b1f
Priyanshurc <priyanshurc987@gmail.com>
2025-01-04 04:16:57 +0530

    Initial commit

---
+++ backend/src/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Routes
+app.get("/", (req, res) => res.send("API is running!"));
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
